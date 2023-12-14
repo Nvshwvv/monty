0aa36f794645c9ef7348db60b9e6924f23ec5624
Nvshwvv <n7216069@gmail.com>
2023-12-14 14:38:45 -0800

    monty project

---
+++ Deal_with_strings2.c
@@ -0,0 +1,49 @@
+#include "monty.h"
+
+/**
+ * rotl - function that Rotates the first node of the stack to the bottom.
+ * @stack: Pointer
+ * @ln: Interger representing the line number of of the opcode.
+ */
+void rotl(stack_t **stack, __attribute__((unused))unsigned int ln)
+{
+	stack_t *tmp;
+
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+		return;
+
+	tmp = *stack;
+	while (tmp->next != NULL)
+		tmp = tmp->next;
+
+	tmp->next = *stack;
+	(*stack)->prev = tmp;
+	*stack = (*stack)->next;
+	(*stack)->prev->next = NULL;
+	(*stack)->prev = NULL;
+}
+
+
+/**
+ * rotr - function that Rotates the last node of the stack to the top.
+ * @stack: Pointer
+ * @ln: Interger representing the line number
+ */
+void rotr(stack_t **stack, __attribute__((unused))unsigned int ln)
+{
+	stack_t *tmp;
+
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+		return;
+
+	tmp = *stack;
+
+	while (tmp->next != NULL)
+		tmp = tmp->next;
+
+	tmp->next = *stack;
+	tmp->prev->next = NULL;
+	tmp->prev = NULL;
+	(*stack)->prev = tmp;
+	(*stack) = tmp;
+}
